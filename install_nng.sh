e86101f5d3ca8de307e17a5d86c218a1dcdf76a0
zwyang <yangziwei@me.com>
2022-05-14 22:38:44 +0800

    ubuntu install nng updated

--- install_nng.sh
+++ install_nng.sh
@@ -1,10 +1,11 @@
 # For NNG v1.5.2.tar.gz on macos m1
 cd $HOME/Proj/nng/
 rm -rf build
+mkdir build
+cd build
 
-brew install mbedTLS
+brew install mbedTLS # sudo apt install libmbedtls-dev
 
-mkdir build && \
-	cmake -DNNG_ENABLE_TLS=ON --install-prefix=$HOME/install  .. && \
+cmake -DNNG_ENABLE_TLS=ON --install-prefix=$HOME/install  .. && \
 	make && \
 	make install
