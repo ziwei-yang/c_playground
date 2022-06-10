018397b01272dff37804f7a1bab6d40a3b1807c5
ziwei yang <yangziwei@me.com>
2022-06-10 14:44:10 +0800

    build.sh compatiable for macos 11 and 12

--- build.sh
+++ build.sh
@@ -1,14 +1,39 @@
+os=$( uname )
+[[ $os != 'Darwin' ]] && echo "build.sh for macos only"
+
+HOMEBREW=
+[ -d /usr/local/Cellar ] && HOMEBREW=/usr/local/Cellar # macos 11.6+
+[ -d /opt/homebrew/Cellar ] && HOMEBREW=/opt/homebrew/Cellar # macos 12+
+
+mbedtls_home=
+for path in $HOMEBREW/mbedtls/* ; do
+	mbedtls_home=$path
+	break
+done
+
+glib_home=
+for path in $HOMEBREW/glib/* ; do
+	glib_home=$path
+	break
+done
+
+hiredis_home=
+for path in $HOMEBREW/hiredis/* ; do
+	hiredis_home=$path
+	break
+done
+
 gcc \
 	-I /usr/local/include \
 	-I $HOME/install/include \
-	-I /opt/homebrew/Cellar/mbedtls/3.1.0/include \
-	-I /opt/homebrew/Cellar/glib/2.72.1/include/glib-2.0 \
-	-I /opt/homebrew/Cellar/glib/2.72.1/lib/glib-2.0/include/\
-	-I /opt/homebrew/Cellar/hiredis/1.0.2/include/hiredis \
+	-I $mbedtls_home/include \
+	-I $glib_home/include/glib-2.0 \
+	-I $glib_home/lib/glib-2.0/include/\
+	-I $hiredis_home/include/hiredis \
 	-L /usr/local/lib \
 	-L $HOME/install/lib \
-	-L /opt/homebrew/Cellar/mbedtls/3.1.0/lib \
-	-L /opt/homebrew/Cellar/glib/2.72.1/lib \
-	-L /opt/homebrew/Cellar/hiredis/1.0.2/lib \
+	-L $mbedtls_home/lib \
+	-L $glib_home/lib \
+	-L $hiredis_home/lib \
 	-lnng -lpthread -lmbedtls -lmbedx509 -lmbedcrypto -lglib-2.0 -lhiredis \
 	$@
