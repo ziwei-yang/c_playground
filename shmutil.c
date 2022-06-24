753009391b44ef4d8b06f4156f109d102d9436da
ziwei yang <yangziwei@me.com>
2022-06-24 22:15:25 +0800

    shmutil added

---
+++ shmutil.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <glib.h>
+#include <time.h>
+
+#include "urn.h"
+
+int main(int argc, char **argv) {
+	if (argc <= 1)
+		return URN_FATAL("args: shmkey del/resize [subargs]", EINVAL);
+	if (strcasecmp(argv[1], "-v") == 0) {
+		URN_LOG("Compiled at " __TIMESTAMP__);
+		return 0;
+	} else if (strcasecmp(argv[1], "-h") == 0) {
+		URN_LOG("args: shmkey del/resize [subargs]");
+		return 0;
+	}
+
+	int rv = 0;
+	struct shmid_ds ds;
+	int shmkey = strtol(argv[1], NULL, 16);
+
+	int shmid = shmget(shmkey, 0, 0);
+	URN_LOGF("shmkey 0x%08x shmid got: %d", shmkey, shmid);
+	if (shmid < 0) {
+		perror("shmget error");
+		return errno;
+	}
+
+	if (strcasecmp(argv[2], "stat") == 0) {
+		rv = shmctl(shmid, IPC_STAT, &ds);
+		if (errno != 0)
+			perror("shmctl IPC_STAT error");
+		URN_INFOF("\tIPC_STAT rv=%d", rv);
+	} else if (strcasecmp(argv[2], "del") == 0) {
+		rv = shmctl(shmid, IPC_RMID, NULL);
+		if (errno != 0)
+			perror("shmctl IPC_RMID error");
+		URN_INFOF("\tIPC_RMID rv=%d", rv);
+	}
+
+	return 0;
+}
