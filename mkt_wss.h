5a688ab81fbd76d8ba91f03a9b42bc0f372a5535
ziwei yang <yangziwei@me.com>
2022-07-02 15:19:59 +0800

    macro PUB_REDIS replaced by pub_redis, controlled by ENV

--- mkt_wss.h
+++ mkt_wss.h
@@ -23,10 +23,6 @@
 #define MAX_PAIRS 512
 #endif
 
-#ifndef PUB_NO_REDIS
-#define PUB_REDIS
-#endif
-
 #ifndef WRITE_NO_SHRMEM
 #define WRITE_SHRMEM
 #endif
@@ -122,6 +118,7 @@ char         *pub_odbk_key_arr[MAX_PAIRS]; // kv snapshot
 char         *pub_tick_chn_arr[MAX_PAIRS];
 char         *pub_tick_key_arr[MAX_PAIRS]; // kv snapshot
 redisContext *redis;
+bool          pub_redis = false;
 
 ///////////// write to SHRMEM //////////////
 urn_odbk_mem *odbk_shmptr = NULL;
@@ -149,6 +146,13 @@ void timeout_sigalrm_handler(int sig) {
 }
 
 int main(int argc, char **argv) {
+	if (getenv("URN_SPIDER_PUB_REDIS") != NULL) {
+		URN_LOG("Enable  pub_redis");
+		pub_redis = true;
+	} else {
+		URN_LOG("Disable pub_redis");
+	}
+
 	alarm(max_msg_interval); // set new timeout
 	signal(SIGALRM, timeout_sigalrm_handler);
 
@@ -196,12 +200,12 @@ int main(int argc, char **argv) {
 	URN_INFOF("nng_tls_engine_name %s", nng_tls_engine_name());
 	URN_INFOF("nng_tls_engine_description %s", nng_tls_engine_description());
 
-#ifdef PUB_REDIS
+	if (pub_redis) {
 	urn_func_opt verbose_opt = {.verbose=1,.silent=0};
 	rv = urn_redis(&redis, getenv("REDIS_HOST"), getenv("REDIS_PORT"), getenv("REDIS_PSWD"), &verbose_opt);
 	if (rv != 0)
 		return URN_FATAL("Error in init redis", rv);
-#endif
+	}
 #ifdef WRITE_SHRMEM
 	rv = urn_odbk_shm_init(true, exchange, &odbk_shmptr);
 	if (rv != 0)
@@ -386,7 +390,7 @@ static int broadcast() {
 	bool write_snapshot = false;
 	long long listener_ttl_ct = 0;
 
-#ifdef PUB_REDIS
+	if (pub_redis) {
 	URN_GO_FINAL_ON_RV(redis->err, "redis context err");
 	/* only collect replies from last time */
 	long long listener_ct = 0;
@@ -415,7 +419,7 @@ static int broadcast() {
 		URN_DEBUGF_C(YELLOW, "write_snapshot %ld %ld, %ld, %ld",
 			_tmp_clock.tv_sec, (_tmp_clock.tv_sec & 7),
 			_tmp_clock.tv_nsec, (_tmp_clock.tv_nsec & 65535));
-#endif
+	}
 
 	/* broadcast this time */
 	int data_ct = 0;
@@ -424,15 +428,15 @@ static int broadcast() {
 		if ((!write_snapshot) && (newodbk_arr[pairid] <= 0))
 			continue;
 
-#ifdef PUB_REDIS
-	#ifdef PUB_LESS_ON_ZERO_LISTENER
+#ifdef PUB_LESS_ON_ZERO_LISTENER
+		if (pub_redis) {
 		if ((!write_snapshot) && (brdcst_listener_arr[pairid] == 0)) {
 			// Publish every 2s if no listener last time.
 			if (brdcst_t_arr[pairid].tv_sec + 2 > brdcst_t.tv_sec)
 				continue;
 			brdcst_t_arr[pairid].tv_sec = brdcst_t.tv_sec;
 		}
-	#endif
+		}
 #endif
 
 		URN_DEBUGF("to broadcast %s %d/%d, updates %d bids %d asks %d",
@@ -442,7 +446,7 @@ static int broadcast() {
 			continue;
 		brdcst_stat_ct ++;
 
-#ifdef PUB_REDIS
+		if (pub_redis) {
 		int ct = 0;
 		*(s+ct) = '['; ct++;
 		ct += sprintf_odbk_json(s+ct, bids_arr[pairid]);
@@ -461,7 +465,7 @@ static int broadcast() {
 			URN_DEBUGF("also snapshot to %s", pub_odbk_key_arr[pairid]);
 			redisAppendCommand(redis, "SET %s %s", pub_odbk_key_arr[pairid], s);
 		}
-#endif
+		}
 		newodbk_arr[pairid] = 0; // reset new odbk ct;
 		brdcst_pairs[data_ct] = pairid;
 		data_ct ++;
@@ -505,15 +509,15 @@ static int broadcast() {
 			brdcst_interval_ms --;
 		}
 
-#ifdef PUB_REDIS
+		if (pub_redis) {
 		URN_LOGF_C(GREEN, "--> %d/s %dr/s in %d, every %ldms, %4.2fms > %d chn, last: %lld ears %s",
 			speed, brdcst_stat_rd_ct/diff, diff, brdcst_interval_ms,
 			(float)cost_us/1000.f, data_ct, listener_ttl_ct,
 			(write_snapshot ? "FULL" : ""));
-#else
+		} else {
 		URN_LOGF_C(GREEN, "--> %d/s %dr/s in %d, every %ldms",
 			speed, brdcst_stat_rd_ct/diff, diff, brdcst_interval_ms);
-#endif
+		}
 		brdcst_stat_rd_ct = 0;
 		brdcst_stat_ct = 0;
 		brdcst_stat_t = brdcst_t.tv_sec;
