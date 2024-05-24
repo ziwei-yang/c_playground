fd8d40f11ed457a4ae57a30e042a3448ee1f7e77
zwyang <yangziwei@me.com>
2024-05-24 11:59:51 -0700

    detect_arbitrage_pattern_c back into URN_CORE module

--- ruby_ext_core/mkt.c
+++ ruby_ext_core/mkt.c
@@ -20,13 +20,13 @@ void Init_urn_core() {
 	ab3_init();
 
 	VALUE urn_core_module = rb_define_module("URN_CORE");
+	rb_define_method(urn_core_module, "detect_arbitrage_pattern_c", method_detect_arbitrage_pattern, 1);
 
 	VALUE cMathUtil = rb_define_module_under(urn_core_module, "MathUtil");
 	rb_define_method(cMathUtil, "diff", rb_diff, 2);
 	rb_define_method(cMathUtil, "stat_array", rb_stat_array, 1);
 	rb_define_method(cMathUtil, "rough_num", rb_rough_num, 1);
 	rb_define_method(cMathUtil, "format_num", rb_format_num, -1);
-	rb_define_method(cMathUtil, "detect_arbitrage_pattern_c", method_detect_arbitrage_pattern, 1);
 
 	order_bind(urn_core_module);
 	order_util_bind(urn_core_module);
